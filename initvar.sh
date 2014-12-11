baacc1146be09fc13b3e2be904c79064edd92272
Enrico Gaffo <enrico.gaffo@gmail.com>
2014-12-11 16:35:28 +0100

    renamed project builder SConscript. Updated template init environment variables

--- initvar.sh
+++ initvar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export SEGEMEHL_HOME="/blackhole/enrico/circular/tools/segemehl_0_1_9/segemehl/"
+export SEGEMEHL_HOME="/home/enrico/tools/segemehl/"
 export SEGEMEHL_INDEX="/blackhole/enrico/circular/tools/segemehl_indexes/hg38/hg38.idx"
 export GENOME_FASTA="/blackhole/enrico/circular/tools/segemehl_indexes/hg38/chr*.fa"
 export SRATOOLKIT_HOME="/home/enrico/tools/sra/sratoolkit.2.3.5-2-ubuntu64/bin/"
-export SAMTOOLS_HOME="/blackhole/enrico/circular/tools/samtools-1.1/"
+export SAMTOOLS_HOME="/home/enrico/tools/samtools/"
 export FASTX_TOOLKIT="$HOME/tools/fastx_toolkit/bin"
 export TRIMMOMATIC="/home/enrico/tools/trimmomatic/trimmomatic-0.32.jar"
 export JUNK2_HOME="/home/enrico/tools/junk2"
