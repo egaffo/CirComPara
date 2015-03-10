8f4c3e7afc2a7872bebc2a90dabaaeb742fe9043
Enrico Gaffo <enrico.gaffo@gmail.com>
2015-03-10 14:56:15 +0100

    updated env vars initialisation template

--- initvar.sh
+++ initvar.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-export SEGEMEHL_HOME="/home/enrico/tools/segemehl/"
-export SEGEMEHL_INDEX="/blackhole/enrico/circular/tools/segemehl_indexes/hg38/hg38.idx"
-export GENOME_FASTA="/blackhole/enrico/circular/tools/segemehl_indexes/hg38/chr*.fa"
-export SRATOOLKIT_HOME="/home/enrico/tools/sra/sratoolkit.2.3.5-2-ubuntu64/bin/"
-export SAMTOOLS_HOME="/home/enrico/tools/samtools/"
-export FASTX_TOOLKIT="$HOME/tools/fastx_toolkit/bin"
-export TRIMMOMATIC="/home/enrico/tools/trimmomatic/trimmomatic-0.32.jar"
 export JUNK2_HOME="/home/enrico/tools/junk2"
-export CUFFLINKS_HOME="/home/enrico/tools/cufflinks"
+
+SEGEMEHL_HOME="/home/enrico/tools/segemehl/"
+SRATOOLKIT_HOME="/home/enrico/tools/sra/sratoolkit.2.3.5-2-ubuntu64/bin/"
+SAMTOOLS_HOME="/home/enrico/tools/samtools/"
+FASTX_TOOLKIT="$HOME/tools/fastx_toolkit/bin"
+TRIMMOMATIC="/home/enrico/tools/trimmomatic/trimmomatic-0.32.jar"
+CUFFLINKS_HOME="/home/enrico/tools/cufflinks"
 
 export PATH="$CUFFLINKS_HOME:$JUNK2_HOME/bin:$FASTX_TOOLKIT:$SEGEMEHL_HOME:$SRATOOLKIT_HOME:$SAMTOOLS_HOME:$PATH"
