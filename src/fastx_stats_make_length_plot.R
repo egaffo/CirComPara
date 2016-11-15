bb2e6926624201e23705830ddedd73950e4c4308
Enrico Gaffo <enrico.gaffo@gmail.com>
2016-11-15 10:24:09 +0100

    Updated code for new version of ggplot2

--- src/fastx_stats_make_length_plot.R
+++ src/fastx_stats_make_length_plot.R
@@ -45,8 +45,9 @@ if(file.access(fastx_stats) == -1){
   fastx$cycle <- ordered(fastx$cycle)
   fastx$length_hist <- cumdiff(fastx$ALL_count)
   
-  lengths_plot <- qplot(x=cycle, y=length_hist, data=fastx, geom="bar", stat="identity", fill=I("blue"), 
-                        main=title, xlab="Read lengths", ylab="Read count") + 
+  lengths_plot <- ggplot(aes(x=cycle, y=length_hist), data=fastx, main=title) +  
+			xlab("Read lengths") + ylab("Read count") + 
+			geom_bar(stat="identity", aes(fill=I("blue"))) + 
                   theme_bw() + plot_template + scale_y_continuous(labels=comma, expand=c(0.01,0)) + 
                   scale_x_discrete(breaks=seq(min(as.integer(fastx$cycle)), max(as.integer(fastx$cycle)), 5))
   ggsave(filename=output, plot=lengths_plot, width=image_width, height=image_height)  
