341eae17f8a61b062e6a63560bb3c052d7277016
Enrico Gaffo <enrico.gaffo@gmail.com>
2016-11-15 10:18:06 +0100

    Install R packs passing the whole list at one time

--- src/install_R_libs.R
+++ src/install_R_libs.R
@@ -1,15 +1,19 @@
 #!/usr/bin/env Rscript
 
-packs <- c("optparse", 
+#prepacks <- c("BH", "svglite")
+#
+#install.packages(prepacks, repos="http://cran.r-project.org", dependencies = T)
+
+packs <- c("BH", "svglite", "optparse", "plyr", 
+	   "scales", "reshape2", 
            "ggplot2", "ggthemes",
-           "scales", "reshape2", 
-           "data.table", "plyr", 
            "RSvgDevice")
 
-for(pack in packs){
-  install.packages(pack, repos="http://cran.r-project.org")
-}
+install.packages(packs, repos="http://cran.r-project.org", dependencies = T)
+
+#for(pack in packs){
+#  install.packages(pack, repos="http://cran.r-project.org", dependencies = T)
+#}
 
 source("http://bioconductor.org/biocLite.R")
-biocLite("DESeq2")
-biocLite("ReportingTools")
+biocLite(pkgs = c("DESeq2", "ReportingTools"))
