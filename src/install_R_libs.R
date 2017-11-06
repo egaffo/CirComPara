65aaa017dc0b90617b4322a5a280b67935389aa2
Enrico Gaffo <enrico.gaffo@gmail.com>
2017-11-06 23:07:54 +0100

    added DT package in R libs install script

--- src/install_R_libs.R
+++ src/install_R_libs.R
@@ -5,7 +5,7 @@
 #install.packages(prepacks, repos="http://cran.r-project.org", dependencies = T)
 
 packs <- c("BH", "svglite", "optparse", "plyr", 
-	   "scales", "reshape2", 
+	   "scales", "reshape2", "DT",
            "ggplot2", "ggthemes",
            "RSvgDevice", "rmarkdown", "knitr", "VennDiagram")
 
