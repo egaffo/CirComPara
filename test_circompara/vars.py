99d583d02b8d27f64a13743c4180cb0df2c8a2ce
Enrico Gaffo <enrico.gaffo@gmail.com>
2018-12-07 20:12:46 +0100

    added example instantiation of parameters in var.py and set new defaults for test

--- test_circompara/vars.py
+++ test_circompara/vars.py
@@ -4,13 +4,38 @@ ANNOTATION      = '../annotation/CFLAR_HIPK3.gtf'
 CPUS            = "2"
 PREPROCESSOR    = "trimmomatic"
 PREPROCESSOR_PARAMS = "MAXINFO:40:0.5 LEADING:20 TRAILING:20 SLIDINGWINDOW:4:30 MINLEN:35 AVGQUAL:30"
-CIRCRNA_METHODS = "ciri,findcirc,testrealign,"\
+CIRCRNA_METHODS = "dcc,ciri,findcirc,testrealign,"\
                   "circexplorer2_star,circexplorer2_segemehl,"\
                   "circexplorer2_bwa,circexplorer2_tophat"
 
 ## aligners' custom parameters
-#BWA_PARAMS = ['-T', '19', '-c', '2']
+HISAT2_EXTRA_PARAMS = "--rna-strandness RF"
+#BWA_PARAMS = ['-T', '19', '-c', '1']
 #SEGEMEHL_PARAMS = ['-M','1']
-#TOPHAT_PARAMS = ['--zpacker','pigz', '--max-multihits', '2']
-#STAR_PARAMS = ['--outFilterMultimapNmax', '2' ] 
+#TOPHAT_PARAMS = ['--zpacker','pigz', '--max-multihits', '1']
+## parameters used in DCC manual example
+#STAR_PARAMS = ['--outFilterMultimapNmax', '20', 
+#               '--outSJfilterOverhangMin', '15', '15', '15', '15',
+#               '--alignSJoverhangMin', '15',
+#               '--alignSJDBoverhangMin', '15',
+#               '--seedSearchStartLmax', '30',
+#               '--outFilterScoreMin', '1',
+#               '--outFilterMatchNmin', '1',
+#               '--outFilterMismatchNmax', '2',
+#               '--chimSegmentMin', '15',
+#               '--chimScoreMin', '15',
+#               '--chimScoreSeparation', '10',
+#               '--chimJunctionOverhangMin', '15'] 
+
+## pre-computed index and annotation files 
+#GENOME_INDEX="../indexes/hisat2/CFLAR_HIPK3"
+#SEGEMEHL_INDEX="../indexes/segemehl/CFLAR_HIPK3.idx"
+#BWA_INDEX="../indexes/bwa/CFLAR_HIPK3"
+#BOWTIE2_INDEX="../indexes/bowtie2/CFLAR_HIPK3"
+#BOWTIE_INDEX="../indexes/bowtie/CFLAR_HIPK3"
+#STAR_INDEX="../indexes/star/CFLAR_HIPK3"
+#GENEPRED="../annotation/CFLAR_HIPK3.genePred.wgn"
+
+#DCC_EXTRA_PARAMS = ['-fg', '-M']
+#LIN_COUNTER = 'dcc' #'ccp'
 
