5f0cef4a3ea126d520ab2dc6029841aa9adffd3a
Enrico Gaffo <enrico.gaffo@gmail.com>
2019-01-08 15:26:07 +0100

    added option to append suffix to read ids

--- src/trim_read_header.py
+++ src/trim_read_header.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python
 
-import os,sys
-from optparse import *
-import gzip
+import os, sys, argparse, gzip
 from collections import namedtuple
-
-usage = """
-
-  %prog <reads.fastq.gz> > trimmed_header_reads.fastq
-
-Reduce read name to ID only, discard description if present. 
-Can handle gzipped files. Set '-' to process stdin input stream.
-"""
-
-parser = OptionParser(usage = usage)
-options,args = parser.parse_args()
 
 def fq_blocks(lines):
     """
@@ -37,18 +24,34 @@ def fq_blocks(lines):
     except StopIteration:
         pass
 
-## handle compressed files 
-if args[0].endswith('.gz'):
-    f = gzip.open(args[0], 'rb')
-elif args[0] == "-":
-    f = sys.stdin
-else:
-    f = open(args[0], 'r')
+if __name__ == '__main__':
 
-for fq in fq_blocks(f):
+    usage = """
+            Reduce read name to ID only, discard description if present.
+            Optionally, append suffix to read ID.
+            """
+
+    parser = argparse.ArgumentParser(description = usage, 
+                                     formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('input', default = '-', help = 'A FASTQ file or stream (default -)')
+    parser.add_argument('-s', '--suffix', type = str, dest = 'suffix', 
+                        default = '',
+                        help = 'Suffix to append to read ID. F.i. "\\1" or "\\2" '\
+                               'to discriminate first/second read mates')
+    args = parser.parse_args()
+
+    ## handle compressed files 
+    if args.input.endswith('.gz'):
+        f = gzip.open(args.input, 'rb')
+    elif args.input == '-':
+        f = sys.stdin
+    else:
+        f = open(args.input, 'r')
     
-    print fq.name.split(" ")[0]
-    print fq.seq
-    print fq.plus
-    print fq.qual
+    for fq in fq_blocks(f):
+        
+        print fq.name.split(" ")[0] + args.suffix
+        print fq.seq
+        print fq.plus
+        print fq.qual
  
