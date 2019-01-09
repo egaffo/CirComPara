99df662c7cfa406e2fa3eff64d34186326ae1b03
Enrico Gaffo <enrico.gaffo@gmail.com>
2019-01-09 12:19:17 +0100

    discriminate first and second reads in pairs by attaching suffixes to linearly unmapped read IDs

--- src/trim_read_header.py
+++ src/trim_read_header.py
@@ -24,34 +24,53 @@ def fq_blocks(lines):
     except StopIteration:
         pass
 
+def output_fixed(filename, suffix):
+
+    ## handle compressed files 
+    if filename.endswith('.gz'):
+        f = gzip.open(filename, 'rb')
+    elif filename == '-':
+        f = sys.stdin
+    else:
+        f = open(filename, 'r')
+    
+    for fq in fq_blocks(f):
+        
+        print fq.name.split(" ")[0] + suffix
+        print fq.seq
+        print fq.plus
+        print fq.qual
+
 if __name__ == '__main__':
 
     usage = """
             Reduce read name to ID only, discard description if present.
-            Optionally, append suffix to read ID.
+            Optionally, append suffix to read ID. It can concatenate 
+            paired-end reads and append '1' and '2' suffixes by default.
             """
 
     parser = argparse.ArgumentParser(description = usage, 
                                      formatter_class = argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('input', default = '-', help = 'A FASTQ file or stream (default -)')
+    parser.add_argument('-f', '--mate1', default = '-', required = True,
+                        help = 'A FASTQ file or stream. Gzip\'d file '\
+                               'supported. Use - for stream input')
+    parser.add_argument('-r', '--mate2', default = None, required = False,
+                        help = 'A FASTQ file. Gzip\'d file supported')
     parser.add_argument('-s', '--suffix', type = str, dest = 'suffix', 
                         default = '',
-                        help = 'Suffix to append to read ID. F.i. "\\1" or "\\2" '\
-                               'to discriminate first/second read mates')
+                        help = 'Suffix to append to read ID. '\
+                               'When paired-end reads, the string is '\
+                               'prepended to the default "1" and "2" suffixes')
     args = parser.parse_args()
 
-    ## handle compressed files 
-    if args.input.endswith('.gz'):
-        f = gzip.open(args.input, 'rb')
-    elif args.input == '-':
-        f = sys.stdin
-    else:
-        f = open(args.input, 'r')
+    suffix = args.suffix
+
+    if args.mate2:
+        suffix = args.suffix + '1'
     
-    for fq in fq_blocks(f):
-        
-        print fq.name.split(" ")[0] + args.suffix
-        print fq.seq
-        print fq.plus
-        print fq.qual
- 
+    output_fixed(args.mate1, suffix)
+
+    if args.mate2:
+        suffix = args.suffix + '2'
+        output_fixed(args.mate2, suffix)
+
