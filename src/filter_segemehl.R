af940d38f0389eb850220326b9369cbdfbe802b7
Enrico Gaffo <enrico.gaffo@gmail.com>
2019-01-27 15:28:24 +0100

    changed parameter type for filtering segemehl results: now 'median', 'mean' and 'any' strategies are supported

--- src/filter_segemehl.R
+++ src/filter_segemehl.R
@@ -7,9 +7,14 @@ option_list <- list(
     make_option(c("-i", "--input"), action = "store", type = "character",
                 default = "sample.sngl.bed",
                 help = "Segemehl result table file"),
-    make_option(c("-q", "--minqual"), action = "store", type = "integer",
-                default = 25,
-                help = "Minimum mapping quality of backsplice-reads"),
+    make_option(c("-q", "--minqual"), action = "store", type = "character",
+                default = "median_10",
+                help = paste("Filter function and minimum mapping quality of backsplice-reads. ",
+                       "Function supported: ",
+                       "- median: for each backsplice compute the read mapping median quality and discard backsplices with median quality <= N",
+                       "- mean: as for median, but compute the mean instead",
+                       "- any: first, discard any alignment with mapping quality <= N. Then, report the median as backsplice quality",
+                       sep = "\n")),
     make_option(c("-o", "--count_output"), action = "store", type = "character",
                 default = "splicesites.bed",
                 help = "The filtered circrnas in BED format. The 7th column is just a copy of the score field and reports the read count."),
@@ -18,14 +23,32 @@ option_list <- list(
                 help = "The output file")
 )
 
-parser <- OptionParser(usage = "%prog -i sample.sngl.bed -q 25 -o splicesites.bed",
+parser <- OptionParser(usage = "%prog -i sample.sngl.bed -q median_10 -o splicesites.bed -r sample.circular.reads.bed.gz",
                        option_list = option_list,
-                       description = "Filter segemehl results by quality and compute backsplices' counts")
+                       description = "Filter segemehl results by alignments' quality and compute backsplices' read count")
 arguments <- parse_args(parser, positional_arguments = F)
 input <- arguments$input
 count_output <- arguments$count_output
 reads_output <- arguments$reads_output
-minqual <- arguments$minqual
+min.qual <- arguments$minqual
+
+split.qual.par <- strsplit(min.qual, "_", fixed = T)[[1]]
+if(length(split.qual.par) > 1){
+    qual.func <- split.qual.par[1]
+    if(!qual.func %in% c("median", "mean", "any")){
+        message("Error in evaluating quality function in '", min.qual, "': setting default 'median'")
+        qual.func <- "median"
+    }
+    minqual <- suppressWarnings(as.numeric(split.qual.par[2]))
+    if(is.na(minqual)){
+        message("Error in evaluating quality value in '", min.qual, "': setting default '10'")
+        minqual <- 10
+    }
+}else{
+    message("Error in evaluating quality function '", min.qual, "': setting default 'median_10'")
+    qual.func <- "median"
+    minqual <- 10
+}
 
 # Start and end position indicate the genomic range of the predicted intron.
 # The name has the format (read-group;type;read-name;mate-status), the bed
@@ -33,28 +56,60 @@ minqual <- arguments$minqual
 # 'R' (in case of a regular, collinear split), 'C' (circular split) or 'B' (backsplice)
 
 sege_circ <- fread(cmd = paste0('grep ";B\\|C;" ', input),
-                   header = F, skip = 1)[V5 >= minqual]
+                   header = F, skip = 1)
 
 splicesites.bed <- data.table()
 reads_output.dt <- data.table()
 
+if(qual.func == "any"){
+    sege_circ <- sege_circ[V5 >= minqual]
+}
+
 if(nrow(sege_circ) > 0){
 
-    sege_circ[, c("read.group", "type", "read.name", "mate.status"):=(tstrsplit(V4, ";"))]
-    sege_circ <- sege_circ[, .(multi.mapping = .N, map.qual = median(V5)),
+    sege_circ[, c("read.group", "type", "read.name",
+                  "mate.status"):=(tstrsplit(V4, ";"))][, `:=`(V4 = NULL,
+                                                               read.group = NULL,
+                                                               type = NULL,
+                                                               mate.status = NULL)]
+    ## remove duplicated lines/alignments
+    sege_circ <- sege_circ[, .(multi.mapping = .N,
+                               map.qual = max(V5)),
                            by = .(chr = V1, left = V2,
                                   right = V3, read.name,
                                   strand = V6)]
 
-    splicesites.bed <-
-        sege_circ[, .(n = .N,
-                      median_qual = median(map.qual)),
-                  by = .(chr, left, right,
-                         strand)][, .(chr, left, right, n, median_qual, strand,
-                                      score = n)][order(chr, left,
-                                                        right)]
+    if(qual.func == "any" || qual.func == "median"){
+        splicesites.bed <-
+            sege_circ[, .(n = .N,
+                          map.qual = median(map.qual)),
+                      by = .(chr, left, right,
+                             strand)][, .(chr, left, right, n, map.qual, strand,
+                                          score = n)][order(chr, left,
+                                                            right)]
+    }
 
-    reads_output.dt <- sege_circ[, .(chr, left, right, read.name, map.qual, strand)]
+    if(qual.func == "mean"){
+        splicesites.bed <-
+            sege_circ[, .(n = .N,
+                          map.qual = mean(map.qual)),
+                      by = .(chr, left, right,
+                             strand)][, .(chr, left, right, n, map.qual, strand,
+                                          score = n)][order(chr, left,
+                                                            right)]
+    }
+
+    if(qual.func == "median" || qual.func == "mean"){
+        ## filter by quality
+        splicesites.bed <- splicesites.bed[map.qual >= minqual]
+    }
+
+    ## report selected backsplices' read names
+    # reads_output.dt <- sege_circ[, .(chr, left, right, read.name, map.qual, strand)]
+    reads_output.dt <- sege_circ[splicesites.bed[, .(chr, left, right, strand)],
+                                 on = c("chr", "left", "right",
+                                        "strand")][, .(chr, left, right, read.name,
+                                                       map.qual, strand)]
 }
 
 ## write backsplice counts
