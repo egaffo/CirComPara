3f83916c631a68a85771ecd97e1dc4f8a4b4f1db
Enrico Gaffo <enrico.gaffo@gmail.com>
2019-03-29 22:23:29 +0100

    improved annotation and circrna strand assignemnt: intergenic circrnas whihc are aligned also to a gene in the opposite strand are now assigned to the gene. This partly solve the problem of discordant strand alignments from different read aligners by favoring the annotation to known genes. Also, circrna gene regions notation is now more precise for each circrna

--- src/gene_annotation.R
+++ src/gene_annotation.R
@@ -89,9 +89,6 @@ gene.annotation[, `:=`(circ_id = sub('.*gene_id "([^"]+)".*', "\\1", V9),
 ## collapse/remove circ_id strand
 gene.annotation[, circ_id := sub(":[+-]$", "", circ_id)]
 
-circ.id.strand <- gene.annotation[, .(strand = paste0(unique(V7), collapse = "|")),
-                                  by = .(circ_id)]
-
 ## CIRCRNA TO GENES
 ## make a table with all circrna ids in one column, one per row, and
 ## the corresponding genes in a side column, in a bar-separated list.
@@ -100,18 +97,41 @@ circ_to_genelist <-
     unique(gene.annotation[, .(circ_id, gene_id, gene_name,
                                gene_biotype,
                                gene_region = V12,
-                               gene_strand = V16)])[, .(gene_ids = list(unique(gene_id)),
+                               gene_strand = V16,
+                               circ_strand = V7)])[, .(gene_ids = list(unique(gene_id)),
                                                         gene_names = list(unique(gene_name)),
                                                         gene_biotypes = list(unique(gene_biotype)),
                                                         gene_region = list(unique(gene_region)),
                                                         gene_strand = list(unique(gene_strand))),
-                                                    by = circ_id]
+                                                    by = .(circ_id, circ_strand)]
 circ_to_genelist[, simple_gene_region := sapply(gene_region, simplify.gene.region.list)]
 
 ## Intergenic clusters:
 ## save intergenic circrnas in a BED file to be input to bedtools cluster
 ## which will tell how many cluster (putative new genes) express the circrnas
-intergenic.circs <- circ_to_genelist[simple_gene_region == "intergenic", .(circ_id)]
+# intergenic.circs <- circ_to_genelist[simple_gene_region == "intergenic", .(circ_id)]
+## do not consider as intergenic the circrnas that have been detected in any gene
+## on the opposite strand. This should compensate and fix wrong strand alignments.
+intergenic.circs <-
+    circ_to_genelist[simple_gene_region == "intergenic" &
+                         ! circ_id %in%
+                         circ_to_genelist[simple_gene_region != "intergenic",
+                                          circ_id]]
+
+## this could be useful to keep track of intergenic circrnas re-assigned
+## to genes from the opposite strand
+# ambiguously.aligned.circ.ids <-
+#     circ_to_genelist[simple_gene_region == "intergenic" &
+#                          circ_id %in%
+#                          circ_to_genelist[simple_gene_region != "intergenic",
+#                                           circ_id], circ_id]
+
+## collapse intergenic circrans aligned in both the strands
+intergenic.circs <- intergenic.circs[, lapply(.SD, function(x)list(unique(unlist(x)))),
+                                     by = circ_id]
+## remove "false" intergenic and keep only full intergenic circrnas
+circ_to_genelist <- rbindlist(list(circ_to_genelist[simple_gene_region != "intergenic"],
+                                   intergenic.circs), use.names = T)
 
 ## convert into BED format
 intergenic.circs.bed <-
@@ -124,31 +144,44 @@ intergenic.circs.bed <-
                                                                               start,
                                                                               end,
                                                                               circ_id)]
+intergenic.circs[, `:=`(chr = NULL, start = NULL, end = NULL, coords = NULL)]
 
 ## cluster circrnas irrespective of strand
 intergenic.circ.clusters <-
     merge(intergenic.circs.bed,
           get.circ.cluster(intergenic.circs.bed, max.dist = max.dist),
           by = "circ_id")
-intergenic.circ.clusters[, cluster.coords := paste0("CC", cluster, "_",
+intergenic.circ.clusters[, cluster.coords := paste0("CircClust", "_",
                                                     chr, ":", min(start),
                                                     "-", max(end)),
                          by = cluster]
 
-## set gene_id as cluster name for intergenic circrnas
+## set cluster name as gene_name for intergenic circrnas
 circ_to_genelist <-
-    merge(circ_to_genelist, intergenic.circ.clusters[, .(circ_id, cluster.coords)],
+    merge(circ_to_genelist,
+          intergenic.circ.clusters[, .(circ_id, cluster.coords)],
           by = "circ_id",
           all.x = T)[!is.na(cluster.coords),
-                     gene_names := as.list(cluster.coords)][, .(circ_id, gene_ids,
+                     gene_names := as.list(cluster.coords)][, .(circ_id, circ_strand,
+                                                                gene_ids,
                                                                 gene_names, gene_biotypes,
                                                                 gene_region,
                                                                 simple_gene_region,
                                                                 gene_strand)]
 
 ### now, convert the gene lists to characters by separating genes with a bar char
+# ## faster, perhaps?
+# circ_to_genes <-
+#     circ_to_genelist[, lapply(.SD, function(x)paste0(unique(unlist(x)),
+#                                                      collapse = "|")),
+#                      by = circ_id]
 circ_to_genes <-
-    circ_to_genelist[, lapply(.SD, paste.bar), by = circ_id]
+    circ_to_genelist[, lapply(.SD, function(x)list(unique(unlist(x)))),
+                     by = circ_id]
+
+circ_to_genes[, simple_gene_region := lapply(simple_gene_region,
+                                             simplify.gene.region.list)]
+circ_to_genes <- circ_to_genes[, lapply(.SD, paste.bar), by = circ_id]
 
 ## remove intergenic notation when a known host genes is listed
 remove.point <- function(x)sub("\\|\\.|\\.\\|", "", x)
@@ -157,12 +190,6 @@ cols <- c("gene_ids", "gene_names", "gene_biotypes", "gene_strand")
 circ_to_genes[simple_gene_region != "intergenic",
                   (cols) := lapply(.SD, remove.point), #by = circ_id,
                   .SDcols = cols]
-
-## attach circ_id strand
-circ_to_genes <- merge(circ.id.strand, circ_to_genes, by = "circ_id")
-## fix circrna strand by using host gene strand
-circ_to_genes[strand == "-|+", strand := gene_strand]
-circ.id.strand <- circ_to_genes[, .(circ_id, strand)]
 
 ## save the table
 fwrite(x = circ_to_genes,
@@ -176,21 +203,11 @@ gene_to_circ <-
     unique(gene.annotation[, .(circ_id, gene_id,
                                gene_name, gene_biotype,
                                gene_region = V12,
-                               gene_strand = V16)])[, .(gene_region = list(gene_region)),
-                                                    by = .(circ_id, gene_id, gene_name,
-                                                           gene_biotype, gene_strand)]
-
-gene_to_circ <-
-    merge(gene_to_circ,
-          intergenic.circ.clusters[, .(circ_id, cluster.coords)],
-          by = "circ_id", all.x = T)[!is.na(cluster.coords),
-                                     gene_name := cluster.coords][, .(circ_id, gene_id,
-                                                               gene_name, gene_biotype,
-                                                               gene_region, gene_strand)]
-
-## remove rows that were (erroneously) kept because only one end of the
-## backsplice was intergenic
-gene_to_circ <- gene_to_circ[gene_name != "."]
+                               gene_strand = V16,
+                               circ_strand = V7)])[, .(gene_region = list(gene_region)),
+                                                    by = .(circ_id, circ_strand,
+                                                           gene_id, gene_name,
+                                                           gene_biotype, gene_strand)][gene_region != "intergenic"]
 
 ## set simple region labels
 gene_to_circ[, simple_gene_region := sapply(gene_region, simplify.gene.region.list)]
@@ -198,13 +215,18 @@ gene_to_circ[, simple_gene_region := sapply(gene_region, simplify.gene.region.li
 ## convert lists into strings
 gene_to_circ <-
     gene_to_circ[, .(gene_region = paste.bar(gene_region)),
-                 by = .(gene_id, gene_name, circ_id,
+                 by = .(gene_id, gene_name, circ_id, circ_strand,
                         simple_gene_region, gene_biotype, gene_strand)]
 
-## attach circrna strand
-gene_to_circ <- merge(circ.id.strand,
-                      gene_to_circ,
-                      by = "circ_id")
+gene_to_circ <-
+    rbindlist(list(gene_to_circ,
+                   circ_to_genes[simple_gene_region == "intergenic",
+                                 .(circ_id, circ_strand,
+                                   gene_id = gene_ids,
+                                   gene_name = gene_names,
+                                   gene_biotype = gene_biotypes,
+                                   gene_strand, gene_region, simple_gene_region)]),
+                                 use.names = T)
 
 ## save table
 fwrite(x = gene_to_circ,
