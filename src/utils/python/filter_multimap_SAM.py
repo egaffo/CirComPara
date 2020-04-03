baa048880e7ed9d4095ce43f32d4be829e1dd685
Enrico Gaffo <enrico.gaffo@gmail.com>
2020-04-03 15:23:43 +0000

    added script to filter SAM files according to max read multimapping

---
+++ src/utils/python/filter_multimap_SAM.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+'''
+Filter SAM alignments by the number of multple hits of each read
+@author: Enrico Gaffo
+@email: enrico.gaffo@unipd.it
+@license: any use of this script must be approved by Enrico Gaffo
+'''
+
+import argparse, pysam, gzip
+
+if __name__ == '__main__':
+    # just handle input parameters
+    parser = argparse.ArgumentParser(description = \
+                                        '''Filter SAM alignments by the number of multple '''\
+                                        '''hits of each read. Output to stdout.''',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('samfile', type = str, default = '-',
+    	 	    help = 'Alignment file in SAM format. Can read Gzipped compressed SAM files.')
+    parser.add_argument('-m', '--max_multimaps', default = float('inf'), type = int,
+            help = 'Filter out read with more than MAX_MULTIMAPS multiple alignments.')
+    args = parser.parse_args()
+    
+
+    infile = args.samfile
+    # if compressed file use appropriate function
+    if infile.endswith('.gz'):
+        infile = gzip.open(infile, 'r')
+    
+    samfile = pysam.AlignmentFile(infile)
+    
+    outfile = pysam.AlignmentFile("-", "w", template = samfile)
+
+    for read in samfile:
+        if read.get_tag('NH') < args.max_multimaps:
+            outfile.write(read)
+    
